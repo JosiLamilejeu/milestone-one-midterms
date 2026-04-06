28c4f6340fabc612e586fb12a7a58ae955dca2f4
조시 <joshmaromramirez@gmail.com>
2026-04-06 09:17:03 +0800

    Refactor User class to simplify constructor and fields
    Removed unnecessary fields and updated constructor.

--- User.java
+++ User.java
@@ -2,17 +2,11 @@ public class User {
     private final String username;
     private final String password;
     private final String role;
-    private final String rank;
-    private final String race;
-    private final String userClass;
     
-    public User(String username, String password, String role, String rank, String race, String userClass) {
+    public User(String username, String password, String role) {
         this.username = username;
         this.password = password;
         this.role = role;
-        this.rank = rank;
-        this.race = race;
-        this.userClass = userClass;
     }
     
     public String getUsername() {
@@ -26,17 +20,4 @@ public class User {
     public String getRole() {
         return role;
     }
-    
-    public String getRank() {
-        return rank;
-    }
-    
-    public String getRace() {
-        return race;
-    }
-    
-    public String getUserClass() {
-        return userClass;
-    }
-    
 }
